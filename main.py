ad987774aa31f96ce8ef87145146ad28dc745e87
Sebastian Slusarczyk <sslusarczyk87@gmail.com>
2023-12-29 14:21:07 -0500

    Add count

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ lista_zakupow = {
  "Warzywniak": ['Marchew','Seler','Rukola']
 }
 
+count0 = 0
+count1 = 0
 for key, value in lista_zakupow.items():
-    print ('Ide do', key, 'i kupuje tam', value)
-    
+    uppercase_tekst1 = "Ide do"
+    uppercase_tekst2 = "i kupuje tam"
+    uppercase_value = value
+    print (uppercase_tekst1.upper(), key.upper(), uppercase_tekst2.upper(), uppercase_value)
+
+count0 = len (lista_zakupow['Piekarnia'])
+count1 = len(lista_zakupow['Warzywniak'])
+
+print (f"W sumie kupuje {count0+count1} produktow")
