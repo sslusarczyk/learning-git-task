e13f60ca45465ede290b279cd1efbb295c6818a2
Sebastian Slusarczyk <sslusarczyk87@gmail.com>
2023-12-29 13:55:20 -0500

    Add Print

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ lista_zakupow = {
  "Piekarnia": ['Chleb','Paczek','Bulki'],
  "Warzywniak": ['Marchew','Seler','Rukola']
 }
+
+for key, value in lista_zakupow.items():
+    print ('Ide do', key, 'i kupuje tam', value)
+    
