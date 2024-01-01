4c6a1565240d24b8c1dc4c41ef6ef8eb279adfdb
Sebastian Slusarczyk <sslusarczyk87@gmail.com>
2024-01-01 14:17:59 -0500

    Print Upper case fix

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ lista_zakupow = {
 
 count0 = 0
 count1 = 0
-for key, value in lista_zakupow.items():
+for sklep, produkt in lista_zakupow.items():
     uppercase_tekst1 = "Ide do"
     uppercase_tekst2 = "i kupuje tam"
-    uppercase_value = value
-    print (uppercase_tekst1.upper(), key.upper(), uppercase_tekst2.upper(), uppercase_value)
+
+    print (uppercase_tekst1.upper(), sklep.upper(), uppercase_tekst2.upper(), ', '.join([produkt.upper() for produkt in produkt]))
+
 
 count0 = len (lista_zakupow['Piekarnia'])
 count1 = len(lista_zakupow['Warzywniak'])
